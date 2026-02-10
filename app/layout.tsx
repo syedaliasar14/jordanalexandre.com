ef907e4497ab6dfdff24ea8a51d7d08bdf7b86c8
Syed Ali Asar <sa.asar14@gmail.com>
2026-02-09 20:51:47 -0500

    remove SanityLive from layout.tsx to fix editing bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { helveticaNeue, loRes9Wide, loRes21Serif } from "./fonts";
 import { draftMode } from "next/headers";
 import { DisableDraftMode } from "./components/disable-draft-mode";
 import { VisualEditing } from "next-sanity/visual-editing";
-import { SanityLive } from "@/sanity/lib/live";
 
 export const metadata: Metadata = {
   title: "JordanAlexandre.com",
@@ -16,7 +15,6 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
     <html lang="en">
       <body className={`antialiased ${helveticaNeue.variable} ${loRes9Wide.variable} ${loRes21Serif.variable}`}>
         {children}
-        <SanityLive />
         {(await draftMode()).isEnabled && (
           <>
             <DisableDraftMode />
