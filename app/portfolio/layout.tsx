aecc893a6ac869b67ac2c8fce4faa12534ebcd49
Syed Ali Asar <sa.asar14@gmail.com>
2026-02-11 22:18:25 -0500

    mobile test

--- app/portfolio/layout.tsx
+++ app/portfolio/layout.tsx
@@ -4,10 +4,10 @@ import Sidebar from "../components/sidebar";
 
 export default function PortfolioLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-[calc(100dvh)] w-full">
+    <div className="min-h-screen w-full">
       <Sidebar />
       <div className="md:ml-64">
-        <main className="flex flex-col min-h-[calc(100dvh)] w-full items-center mb-20">
+        <main className="flex flex-col min-h-screen w-full items-center mb-20">
           <Header />
           <div className="flex-grow flex flex-col justify-center items-center w-full px-4 md:px-8">
             {children}
